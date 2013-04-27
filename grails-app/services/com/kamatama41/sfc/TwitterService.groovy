f81b4b2b9e970785ae99101bd26fb113da03321d
kamatama41 <shiketaudonko41@gmail.com>
2013-04-28 04:47:41 +0900

    Debug System.getEnv()'s value

--- grails-app/services/com/kamatama41/sfc/TwitterService.groovy
+++ grails-app/services/com/kamatama41/sfc/TwitterService.groovy
@@ -10,7 +10,9 @@ class TwitterService {
 	private final Twitter twitter
 
 	TwitterService(String consumerKey, String consumerSecret, String token, String tokenSecret, env) {
-		logger.warn(env.toString())
+		env.each {
+			logger.error("${it.key}:${it.value}")
+		}
 		def singleton = TwitterFactory.singleton
 		singleton.setOAuthConsumer(consumerKey, consumerSecret)
 		singleton.setOAuthAccessToken(new AccessToken(token, tokenSecret))
