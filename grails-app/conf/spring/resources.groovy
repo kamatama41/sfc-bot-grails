d415234a650c68cfeb969e0b7dbdf5981fd93d89
kamatama41 <shiketaudonko41@gmail.com>
2013-04-28 03:37:45 +0900

    Debug System.getEnv()'s value

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,6 +3,9 @@ import com.kamatama41.sfc.TwitterService
 // Place your Spring DSL code here
 beans = {
 	def env = System.getenv()
+	env.each {
+		println "${it.key}=${it.value}"
+	}
 	def consumerKey = env['TWITTER_CONSUMER_KEY']
 	def consumerSecret = env['TWITTER_CONSUMER_SECRET']
 	def token = env['TWITTER_TOKEN']
